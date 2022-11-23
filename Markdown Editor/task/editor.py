b4926323d637beafdd4511b1ad0a30632be471d5
Acestus <architect@acestus.com>
2022-11-23 17:00:17 -0600

    added stage 2 of 5

--- Markdown Editor/task/editor.py
+++ Markdown Editor/task/editor.py
@@ -1,9 +1,28 @@
-# write your code here
-print("# John Lennon")
-print("or ***John Winston Ono Lennon*** was one of *The Beatles*.")
-print("Here are the songs he wrote I like the most:")
-print("* Imagine")
-print("* Norwegian Wood")
-print("* Come Together")
-print("* In My Life")
-print("* ~~Hey Jude~~ (that was *McCartney*)")
+while True:
+    prompt = input("Choose a formatter: ")
+    prompt = prompt.replace(" ", "")
+    if prompt == "plain":
+        print("Text without formatting")
+    elif prompt == "bold":
+        print("**Text**")
+    elif prompt == "italic":
+        print("*Text*")
+    elif prompt == "inline-code":
+        print("`Text`")
+    elif prompt == "link":
+        print("[Text](link)")
+    elif prompt == "header":
+        print("Text")
+    elif prompt == "ordered-list":
+        print("1. Text")
+    elif prompt == "unordered-list":
+        print("* Text")
+    elif prompt == "new-line":
+        print("")
+    elif prompt == "!help":
+        print("Available formatters: plain bold italic inline-code link header unordered-list ordered-list new-line")
+        print("Special commands: !help !done")
+    elif prompt == "!done":
+        break
+    else:
+        print("Unknown formatting type or command")
